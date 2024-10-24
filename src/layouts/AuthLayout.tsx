9d59a86291c7e96252a16f61003dff295ef25120
Pablo Scherpa <desarrollador@pabloscherpa.com.ar>
2024-10-24 07:06:19 -0300

    text center Outlet

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout() {
           <Link to={"/"}>
             <Logo />
           </Link>
-          <div className="mt-10">
+          <div className="mt-10 text-center">
             <Outlet />
           </div>
         </div>
