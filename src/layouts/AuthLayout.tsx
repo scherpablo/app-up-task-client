c1151d399e5ededf8ec6c59cca82e56e2abdd1e8
Pablo Scherpa <desarrollador@pabloscherpa.com.ar>
2024-10-24 06:39:14 -0300

    link to Logo AuthLayout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,22 +1,21 @@
-import Logo from '@/components/Logo'
-import { Outlet } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
+import Logo from "@/components/Logo";
+import { Link, Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 export default function AuthLayout() {
   return (
     <>
-        <div className='bg-gray-800 min-h-screen'>
-            <div className='py-10 lg:py-20 mx-auto w-[450px]'>
-                <Logo />
-                <div className='mt-10'>
-                    <Outlet />
-                </div>
-            </div>
+      <div className="bg-gray-800 min-h-screen">
+        <div className="py-10 lg:py-20 mx-auto w-[450px]">
+          <Link to={"/"}>
+            <Logo />
+          </Link>
+          <div className="mt-10">
+            <Outlet />
+          </div>
         </div>
-        <ToastContainer
-            pauseOnHover={false}
-            pauseOnFocusLoss={false}
-        />
+      </div>
+      <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
     </>
-  )
+  );
 }
