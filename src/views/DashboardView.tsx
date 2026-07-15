714aed72894038083313519bbe102e2a6a59119b
Paboo Scherpa <pabloscherpa@gmail.com>
2026-07-15 16:53:54 -0300

    Ocultar boton de crear proyecto para usuarios que no son el owner

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -29,12 +29,14 @@ export default function DashboardView() {
       <h1 className="text-5xl font-black">Mis Proyectos</h1>
       <p className="text-2xl font-light text-gray-500 mt-5">Maneja y administra tus proyectos</p>
 
-      <nav className="my-5 ">
-        <Link
-          className=" bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-          to='/projects/create'
-        >Nuevo Proyecto</Link>
-      </nav>
+      {user.email === import.meta.env.VITE_OWNER_EMAIL && (
+        <nav className="my-5 ">
+          <Link
+            className=" bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            to='/projects/create'
+          >Nuevo Proyecto</Link>
+        </nav>
+      )}
 
       {data.length ? (
         <ul role="list" className="divide-y divide-gray-100 border border-gray-100 mt-10 bg-white shadow-lg">
