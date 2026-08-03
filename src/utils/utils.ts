8f7b2707d36d83b447e548bbafc1411b2a297acd
Paboo Scherpa <pabloscherpa@gmail.com>
2026-08-03 19:22:11 -0300

    Agregar hora a las fechas mostradas en tareas y notas

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,7 +3,9 @@ export function formatDate(isoString: string) : string {
     const formatter = new Intl.DateTimeFormat('es-ES', {
         year: 'numeric',
         month: 'long',
-        day: 'numeric'
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
     })
     return formatter.format(date)
 }
